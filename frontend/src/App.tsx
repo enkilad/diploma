d9dda1e7dc69f4b5466d6869a9b686726636de2e
Nathan <enkilad@gmail.com>
2021-05-15 02:44:15 +0300

    init 3

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { TableContainer } from './TableContainer';
+
+export const App = () => {
+
+  return (
+    <div>
+      <TableContainer />
+    </div>
+  );
+};
---
+++ frontend/src/TableContainer.tsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { loadData, ProcessedData } from './loadData';
+
+export const TableContainer = () => {
+  const [tableRows, setTableRows] = useState<ProcessedData[]>([]);
+  console.log('tableRows :>> ', tableRows);
+
+  useEffect(() => {
+    const load = async () => {
+      const data = await loadData();
+
+      setTableRows(data);
+
+      
+    };
+
+    load();
+  }, []);
+
+  if (!tableRows) return <div>Error!</div>;
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Category</th>
+          <th>Classification</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tableRows?.map((row) => {
+          return (
+            <tr key={row.id}>
+              <td>{row.name}</td>
+              <td>{row.category}</td>
+              <td>{row.classification}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
